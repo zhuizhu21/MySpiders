d971e59f9a6d83a9a9a2eb08865d465fc6ca3ef3
刘乾 <41061359+zhuizhu21@users.noreply.github.com>
2020-05-06 22:05:25 +0800

    修改标题|添加到服务端定时任务

--- tyutyzw.py
+++ tyutyzw.py
@@ -1,37 +1,17 @@
-import requests,pandas
+import requests
 from bs4 import BeautifulSoup
 
-
-
+# 访问太原理工大学研招首页
 URL_INFO="http://yz.tyut.edu.cn/sszs/fsdjlqzl.htm"
-
-# res=pandas.read_html(URL_INFO)
-
-# info=res[11]
-
-# obj=info[0][1]
-
-# obj.replace('·','')
-# print(obj.split(' '))
-
-# for item in info:
-#     print(item)
-
-# i=0
-# for item in res:
-#     print(i,end='=>            \t')
-#     print(item)
-#     i+=1
-
 html=requests.get(URL_INFO)
+
+# 修改响应编码
 html.encoding = "UTF-8-SIG"
-
-
 text=html.text.encode('utf-8')
 
 bsObj=BeautifulSoup(text,'lxml')
 
-
+#解析标签  获取特定的a标签和span标签  a标签为通知标题，span标签为时间
 titleAll=bsObj.find_all('a',{'style':'TEXT-DECORATION: none; text-align:left;'})
 
 timeAll=bsObj.find_all('span',{"style":"BACKGROUND-COLOR: #efefef"})
@@ -40,10 +20,14 @@ res=""
 for i in range(len(titleAll)):
     title=titleAll[i].get_text()
     time=str(timeAll[i].get_text().encode()).split(' ')[1][-10:]
-    res+=time+' '+title+'\n'
+    # 获取第一条通知作为推送标题
+    if i==0:
+        info=time+' '+title
+    res+='#### '+time+' '+title+'  \n'
 
+
+# server酱消息推送
 URL="https://sc.ftqq.com/SCU96652Tc0c696410a2ae2a2beb33f1bab3fbc815eb21b7f74246.send"
 
-info="2020-05-05 关于公布2020年研招一志愿校内调剂审核结果的通知"
 data={'text':info,'desp':res}
-print(requests.post(URL,data=data))
+requests.post(URL,data=data)
